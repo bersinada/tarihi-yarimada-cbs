ef32e5b43a111c3139e6a821812492170e621b92
Berat Sinan Ada <adaberat7@gmail.com>
2025-12-24 08:42:26 +0300

    Fix: Include virtual environment in deployment artifact

--- startup.sh
+++ startup.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 # Azure App Service Startup Script
-# Bu script Azure'da uygulama başlatılırken çalışır
-
-# Çalışma dizinine geç
 cd /home/site/wwwroot
 
+# Virtual environment'ı aktifleştir
+source antenv/bin/activate
+
 # Gunicorn ile FastAPI uygulamasını başlat
-# PORT: Azure tarafından otomatik atanır (genellikle 8080)
 gunicorn backend.main:app \
     --workers 2 \
     --worker-class uvicorn.workers.UvicornWorker \
